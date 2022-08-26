87777ce655f63fb639dee47916902dffc54c0c10
Andrew <acreyes311@yahoo.com>
2022-08-26 14:29:18 -0700

    start refactoring

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Andrew Reyes
+ * Simple brick breaker game project using SFML.
+ * date: 6-5-22
+ * SFML doc: https://www.sfml-dev.org/documentation/2.5.1/index.php
+ */
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cmath>
+
+int main()
+{
+
+    return 0;
+}
